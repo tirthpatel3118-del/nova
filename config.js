1dcfca46c3cda90a91f6427b6451e203fa39fc90
tirthpatel3118-del <tirthpatel.3118@gmail.com>
2026-05-01 21:11:10 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+window.APP_CONFIG = {
+  appwriteEndpoint: "https://cloud.appwrite.io/v1",
+  appwriteProjectId: "69f36044003919260400",
+  appwriteDatabaseId: "nova",
+  appwriteTables: {
+    subjects: "subjects",
+    chapters: "chapters",
+    assignments: "assignments",
+    uploads: "uploads",
+    siteSettings: "site_settings",
+  },
+  adminUserId: "69f3639a003a8fdf142a",
+  uploaderUserIds: [],
+  driveFolderLink: "https://drive.google.com/drive/folders/1Sn3_dyc_EKlHoeP6HppAgMVmWmO-7TDV?usp=sharing",
+};
